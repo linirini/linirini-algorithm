2dfe565a13c5e78e1059cbc91120198b7844fec8
linirini <101927543+linirini@users.noreply.github.com>
2023-04-15 23:52:17 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 12 ms, Memory: 3312 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<algorithm>
+#include<queue>
+using namespace std;
+//#1697_숨바꼭질
+int N, K, visited[200001];
+int main() {
+	ios_base::sync_with_stdio(0); cout.tie(0); cin.tie(0);
+	cin >> N >> K;
+	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+	pq.push({ 0, N });
+	visited[N] = 1;
+	while (!pq.empty()) {
+		int time = pq.top().first;
+		int now = pq.top().second;
+		pq.pop();
+		if (now == K) {
+			cout<<time;
+			break;
+		}
+		if ((now * 2) >= 0 && (now * 2) <= 200000 && !visited[now * 2]) {
+			pq.push({ time,now * 2 });
+			visited[now * 2] = 1;
+		}
+		if ((now + 1) >= 0 && (now + 1) <= 200000 && !visited[now + 1]) {
+			pq.push({ time+1,now + 1 });
+			visited[now + 1] = 1;
+		}
+		if ((now - 1) >= 0 && (now - 1) <= 200000 && !visited[now - 1]) {
+			pq.push({ time + 1,now - 1 });
+			visited[now - 1] = 1;
+		}
+	}
+}
