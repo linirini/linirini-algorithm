1d0d29a4645574b59b1dbf1f7f94e74508fb5d30
linirini <101927543+linirini@users.noreply.github.com>
2023-04-22 00:00:22 +0900

    Update 왕복.cc

--- 백준/Silver/18311. 왕복/왕복.cc
+++ 백준/Silver/18311. 왕복/왕복.cc
@@ -1,43 +1,25 @@
-#include <iostream>
-#include <vector> 
-#include <algorithm>
-using namespace std; 
-
-int main() 
-{
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
-	int n;
-	long long k; 
-	cin >> n >> k; 
-
-	vector<int> v; 
-	for(int i = 0; i < n; i++){
-		int x; 
-		cin >> x; 
-		v.push_back(x); 
-	}
-
-	bool finished = false; 
-	for(int i = 0; i < n; i++){
-		k -= v[i];
-		if(k < 0){
-			cout << i + 1; 
-			finished = true; 
-			break; 
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<stdlib.h>
+using namespace std;
+//#18311_왕복
+long long N, K,n,dis[200001],sum,in[1000001];
+int main() {
+	ios_base::sync_with_stdio(0); cout.tie(0); cin.tie(0);
+	cin >> N >> K;
+	for (int i = 0; i < N; i++) cin >> in[i];
+	dis[0] = in[0];
+	for(int i=1;i<N;i++)dis[i] = dis[i-1] + in[i];
+	for (int i = N - 1; i >= 0; i--) dis[2 * N - 1 - i] = dis[2*N - i - 2] + in[i];
+	for (int i = 0; i < 2 * N; i++) {
+		if (dis[i] == K) {
+			i < N ? cout << i + 2 : cout << 2 * N - (i + 1);
+			break;
+		}
+		else if (dis[i] > K) {
+			i < N ? cout << i + 1 : cout << 2 * N - i;
+			break;
 		}
 	}
-
-	if(!finished){
-		for(int i = n - 1; i >= 0; i--){
-			k -= v[i]; 
-			if(k < 0){ 
-				cout << i + 1;
-				break; 
-			}
-		}
-	}
-	
-	return 0; 
 }
