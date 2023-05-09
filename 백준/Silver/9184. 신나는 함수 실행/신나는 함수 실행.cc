ee920a317b416dce7bf81d466bf34a8d21683e68
linirini <101927543+linirini@users.noreply.github.com>
2023-05-09 22:36:07 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 8 ms, Memory: 2540 KB -BaekjoonHub

---
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+//#9184_신나는 함수 실행
+int a, b, c;
+int dp[51][51][51];
+int w(int a, int b, int c) {
+	if (a <= 0 || b <= 0 || c <= 0)return 1;
+	else if (a > 20 || b > 20 || c > 20)return w(20, 20, 20);
+	else if (dp[a][b][c] != 0) return dp[a][b][c];
+	else if (a < b && b < c)dp[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+	else dp[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+	return dp[a][b][c];
+}
+int main() {
+	ios_base::sync_with_stdio(0); cout.tie(0); cin.tie(0);
+	while (1) {
+		cin >> a >> b >> c;
+		if (a == -1 && b == -1 && c == -1)return 0;
+		cout << "w(" << a << ", " << b << ", " << c << ") = " << w(a, b, c) << '\n';
+	}
+}
