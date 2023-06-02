5250e3409b64ba334e4a9bc1f045991c3343e9ff
linirini <101927543+linirini@users.noreply.github.com>
2023-06-02 21:25:15 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+//#11722_가장 긴 감소하는 부분 수열
+int N, A[1001], dp[1001], Max;
+int main() {
+	ios_base::sync_with_stdio(0); cout.tie(0); cin.tie(0);
+	cin >> N;
+	for (int i = 0; i < N; i++)cin >> A[i];
+	for (int i = 0; i < N; i++)dp[i] = 1;
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < i; j++)
+			if (A[j] > A[i] && dp[i] < dp[j] + 1) dp[i] = dp[j] + 1;
+	sort(dp, dp + N);
+	cout << dp[N - 1];
+}
