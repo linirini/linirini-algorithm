04f5e41ebe1303deaa82e38d61f021294a3a0066
linirini <101927543+linirini@users.noreply.github.com>
2024-01-04 17:04:49 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 12 ms, Memory: 3332 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.cc
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.cc
@@ -4,46 +4,38 @@
 #include<stack>
 using namespace std;
 //#13913_숨바꼭질 4
-int N, K, visited[200001],par[200001];
+int N, K, parent[200001];
+queue<int>q;
 int main() {
 	ios_base::sync_with_stdio(0); cout.tie(0); cin.tie(0);
 	cin >> N >> K;
-	for (int i = 0; i < 200001; i++)par[i] = i;
-	queue<int>q;
+	for (int i = 0; i < 200001; i++)parent[i] = -1;
 	q.push(N);
 	while (!q.empty()) {
 		int now = q.front();
 		q.pop();
-		if (now == K) {
-			cout << visited[K]<<'\n';
-			break;
-		}
-		if ((now + 1) >= 0 && (now + 1) <= 200000 && !visited[now + 1]) {
+		if (now == K) break;
+		if (now+1<=200000&& parent[now + 1] == -1) {
+			parent[now + 1] = now;
 			q.push(now + 1);
-			visited[now + 1] = visited[now] + 1;
-			par[now + 1] = now;
 		}
-		if ((now - 1) >= 0 && (now - 1) <= 200000 && !visited[now - 1]) {
-			q.push(now - 1);
-			visited[now - 1] = visited[now] + 1;
-			par[now - 1] = now;
-
-		}
-		if ((now * 2) >= 0 && (now * 2) <= 200000 && !visited[now * 2]) {
+		if (now*2 <= 200000&& parent[now * 2] == -1) {
+			parent[now * 2] = now;
 			q.push(now * 2);
-			visited[now * 2] = visited[now] + 1;
-			par[now * 2] = now;
+		}
+		if (now-1>=0&& parent[now - 1] == -1) {
+			parent[now - 1] = now;
+			q.push(now - 1);
 		}
 	}
-	int st = K;
-	stack<int>s;
-	s.push(K);
-	while (st != N) {
-		s.push(par[st]);
-		st = par[st];
+	stack<int>path;
+	while (K != N) {
+		path.push(K);
+		K = parent[K];
 	}
-	while (!s.empty()) {
-		cout << s.top() << ' ';
-		s.pop();
+	cout << path.size()<<'\n'<<N<<' ';
+	while (!path.empty()) {
+		cout << path.top() << ' ';
+		path.pop();
 	}
 }
