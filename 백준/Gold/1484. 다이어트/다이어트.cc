c22d2541ac53afd690138314f4994b09c2eb636a
linirini <101927543+linirini@users.noreply.github.com>
2024-01-07 20:26:36 +0900

    Update 다이어트.cc

--- 백준/Gold/1484. 다이어트/다이어트.cc
+++ 백준/Gold/1484. 다이어트/다이어트.cc
@@ -1,22 +1,26 @@
 #include<iostream>
 #include<algorithm>
+#include<cmath>
 #include<vector>
 using namespace std;
 //#1484_다이어트
-long long G,flag;
-vector<long long>power;
+long long G;
+vector<long long>ans;
 int main() {
 	ios_base::sync_with_stdio(0); cout.tie(0); cin.tie(0);
 	cin >> G;
-	int st = 1, en = 2;
+	long long st = 1,en=2;
 	while (st < en) {
-		if (en * en - st * st > G)st++;
-		else if (en * en - st * st < G)en++;
-		else {
-			flag = 1;
-			cout << en << '\n';
-			en++;
+		if (pow(en, 2) - pow(st, 2) == G) {
+			ans.push_back(en);
+			st++;
 		}
+		else if (pow(en, 2) - pow(st, 2) > G)st++;
+		else if (pow(en, 2) - pow(st, 2) < G)en++;
 	}
-	if(!flag) cout << -1;
+	if (!ans.size()) {
+		cout << -1;
+		return 0;
+	}
+	for (int i = 0; i < ans.size(); i++)cout << ans[i] << '\n';
 }
