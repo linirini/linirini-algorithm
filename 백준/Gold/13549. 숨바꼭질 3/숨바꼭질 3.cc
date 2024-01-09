2633edeaf23312172507e653b3f29738bcc5e08d
linirini <101927543+linirini@users.noreply.github.com>
2024-01-09 17:44:43 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 0 ms, Memory: 2812 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -1,34 +1,25 @@
-#include<iostream>
 #include<algorithm>
+#include<iostream>
 #include<queue>
 using namespace std;
 //#13549_숨바꼭질 3
-int N, K, visited[200001];
+int N, K, visited[100001];
+deque<pair<int,int>>dq;
 int main() {
 	ios_base::sync_with_stdio(0); cout.tie(0); cin.tie(0);
 	cin >> N >> K;
-	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-	pq.push({ 0, N });
-	visited[N] = 1;
-	while (!pq.empty()) {
-		int time = pq.top().first;
-		int now = pq.top().second;
-		pq.pop();
+	dq.push_back({N,0});
+	while (!dq.empty()) {
+		int now = dq.front().first;
+		int t = dq.front().second;
+		dq.pop_front();
+		visited[now] = 1;
 		if (now == K) {
-			cout<<time;
+			cout << t;
 			break;
 		}
-		if ((now * 2) >= 0 && (now * 2) <= 200000 && !visited[now * 2]) {
-			pq.push({ time,now * 2 });
-			visited[now * 2] = 1;
-		}
-		if ((now + 1) >= 0 && (now + 1) <= 200000 && !visited[now + 1]) {
-			pq.push({ time+1,now + 1 });
-			visited[now + 1] = 1;
-		}
-		if ((now - 1) >= 0 && (now - 1) <= 200000 && !visited[now - 1]) {
-			pq.push({ time + 1,now - 1 });
-			visited[now - 1] = 1;
-		}
+		if ((now * 2) <= 100000 && !visited[now * 2]) dq.push_front({now * 2, t});
+		if ((now + 1) <= 100000 && !visited[now + 1]) dq.push_back({ now + 1,t + 1 });
+		if ((now - 1) >= 0 && !visited[now - 1]) dq.push_back({ now - 1,t + 1 });
 	}
 }
