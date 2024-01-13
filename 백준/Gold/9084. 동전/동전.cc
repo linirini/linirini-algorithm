8babcda644dffa1a5a770223fb2acfe22a11b904
linirini <101927543+linirini@users.noreply.github.com>
2024-01-13 23:37:16 +0900

    [Gold V] Title: 동전, Time: 0 ms, Memory: 2100 KB -BaekjoonHub

---
+++ 백준/Gold/9084. 동전/동전.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+//#9084_동전
+long long T, N, M,dp[10001],coin[21];
+int main() {
+	ios_base::sync_with_stdio(0); cout.tie(0); cin.tie(0);
+	cin >> T;
+	while (T--) {
+		cin >> N;
+		for (int i = 1; i <= N; i++) cin >> coin[i];
+		for (int i = 1; i <= 10000; i++) dp[i] = 0;
+		cin >> M;
+		for (int i = 1; i <= N; i++) {
+			dp[coin[i]]++;
+			for (int j = coin[i]+1; j <= M; j++) {
+				dp[j] += dp[j - coin[i]];
+			}
+		}
+		cout << dp[M] << '\n';
+	}
+}
