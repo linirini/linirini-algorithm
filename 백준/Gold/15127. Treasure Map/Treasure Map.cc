b7b4bd51614b797754ddc2e8a35a5d5e652a6257
linirini <101927543+linirini@users.noreply.github.com>
2024-02-06 14:18:47 +0900

    [Gold III] Title: Treasure Map, Time: 16 ms, Memory: 5964 KB -BaekjoonHub

--- 백준/Gold/15127. Treasure Map/Treasure Map.cc
+++ 백준/Gold/15127. Treasure Map/Treasure Map.cc
@@ -3,44 +3,40 @@
 #include <map>
 #include <string>
 using namespace std;
-int n, m;
-int maxDay;
-vector<pair<int, int>> node(1005);
-vector<vector<pair<int, int>>> edge(1005);
-int dp[1005][1005];
-int ans = -1;
+int n, m, a, b, t, maxDay, ans, dp[1001][1001];
+struct Mine {
+    int g;
+    int d;
+};
+Mine mines[1001];
+vector<pair<int, int>> edge[1001];
 int main() {
     cin >> n >> m;
     for (int i = 1; i <= n; i++) {
-        int g, d;
-        cin >> g >> d;
-        node[i] = { g, d };
-        maxDay = max(maxDay, g % d ? g / d + 1 : g / d);
+        cin >> mines[i].g >> mines[i].d;
+        maxDay = max(maxDay, mines[i].g % mines[i].d ? mines[i].g / mines[i].d + 1 : mines[i].g / mines[i].d);
     }
     for (int i = 0; i < m; i++) {
-        int a, b, t;
         cin >> a >> b >> t;
         edge[a].push_back({ b, t });
         edge[b].push_back({ a, t });
     }
-    dp[1][1] = node[1].first;
+    dp[1][1] = mines[1].g;
     ans = dp[1][1];
     for (int i = 1; i <= maxDay; i++) {
         for (int j = 1; j <= n; j++) {
-            if (dp[i][j] > 0) {
-                for (int k = 0; k < edge[j].size(); k++) {
-                    int nextday = i + edge[j][k].second;
-                    if (nextday > maxDay)continue;
-                    int nextnode = edge[j][k].first;
-                    int nextval = node[nextnode].first - ((nextday - 1) * node[nextnode].second);
-                    if (nextval < 0)nextval = 0;
-                    dp[nextday][nextnode] = max(dp[nextday][nextnode],dp[i][j]+ nextval);
-                    ans = max(ans, dp[nextday][nextnode]);
+            if (dp[i][j]) {
+                for (auto next : edge[j]) {
+                    int nextDay = i + next.second;
+                    if (nextDay > maxDay)continue;
+                    int nextMine = next.first;
+                    int nextTreasure = mines[nextMine].g - ((nextDay - 1) * mines[nextMine].d);
+                    if (nextTreasure < 0)nextTreasure = 0;
+                    dp[nextDay][nextMine] = max(dp[nextDay][nextMine],dp[i][j]+ nextTreasure);
+                    ans = max(ans, dp[nextDay][nextMine]);
                 }
             }
         }
     }
-
     cout << ans;
-    return 0;
 }
