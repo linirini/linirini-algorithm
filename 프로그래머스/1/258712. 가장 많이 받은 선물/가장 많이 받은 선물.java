ad803586dd0640bcbf7b2e812f78e0943d04a987
linirini <101927543+linirini@users.noreply.github.com>
2024-03-05 10:40:33 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 5.11 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -2,47 +2,47 @@ import java.util.*;
 class Solution {
     public int solution(String[] friends, String[] gifts) {
         int answer = 0;
-        Map<String,Integer> names = new HashMap<>();
-        int[][] results = new int[friends.length][friends.length];
-        int[] give = new int[friends.length];
-        int[] take = new int[friends.length];
-        int[] presentIndex = new int[friends.length];
-        int[] ans = new int[friends.length];
+        Map<String,Integer> index = new HashMap<>();
         for(int i=0;i<friends.length;i++){
-            names.put(friends[i],i);
+            index.put(friends[i],i);
         }
+        int[][] giveAndTake = new int[friends.length][friends.length];
         for(int i=0;i<gifts.length;i++){
-            String[] tmp = gifts[i].split(" ");
-            results[names.get(tmp[0])][names.get(tmp[1])]++;
+            String give = gifts[i].split(" ")[0];
+            String take = gifts[i].split(" ")[1];
+            giveAndTake[index.get(give)][index.get(take)]++;
         }
-        for(int i=0;i<results.length;i++){
-            for(int j=0;j<results.length;j++){
-                give[i]+=results[i][j];
+        int[] presentPow = new int[friends.length]; 
+        int[] nextPresent = new int[friends.length]; 
+        for(int i=0;i<giveAndTake.length;i++){
+            int give = 0;
+            int take = 0;
+            for(int j=0;j<giveAndTake[i].length;j++){
+                give = give + giveAndTake[i][j];
+                take = take + giveAndTake[j][i];
             }
+            presentPow[i] = give-take;
         }
-        for(int j=0;j<results.length;j++){
-            for(int i=0;i<results.length;i++){
-                take[j]+=results[i][j];
-            }
-        }
-        for(int i=0;i<presentIndex.length;i++){
-            presentIndex[i]=give[i]-take[i];
-        }
-        for(int i=0;i<results.length;i++){
-            for(int j=i+1;j<results.length;j++){
-                if(results[i][j]>results[j][i]){
-                    ans[i]++;
-                }else if(results[i][j]<results[j][i]){
-                    ans[j]++;
-                }else if(results[i][j]==results[j][i]){
-                    if(presentIndex[i]>presentIndex[j])ans[i]++;
-                    else if(presentIndex[i]<presentIndex[j])ans[j]++;
+        for(int i=0;i<giveAndTake.length;i++){
+            for(int j=i+1;j<giveAndTake[i].length;j++){
+                if(giveAndTake[i][j]>giveAndTake[j][i]){
+                    nextPresent[i]++;
+                }
+                else if(giveAndTake[i][j]==giveAndTake[j][i]){
+                    if(presentPow[i]>presentPow[j]){
+                        nextPresent[i]++;
+                    }else if(presentPow[i]<presentPow[j]){
+                        nextPresent[j]++;
+                    }
+                }
+                else if(giveAndTake[i][j]<giveAndTake[j][i]){
+                    nextPresent[j]++;
                 }
             }
         }
-        for(int i=0;i<ans.length;i++){
-            System.out.print(ans[i]);
-            if(answer<ans[i])answer = ans[i];
+        for(int i=0;i<nextPresent.length;i++){
+            System.out.println(nextPresent[i]);
+            answer = Math.max(answer,nextPresent[i]);
         }
         return answer;
     }
