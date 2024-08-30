8fc573a4f20d08c79c92d157c92eb554b309f728
linirini <101927543+linirini@users.noreply.github.com>
2024-08-30 20:05:28 +0900

    [Gold III] Title: ABBC, Time: 4 ms, Memory: 5036 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/25381. ABBC/ABBC.cc
+++ 백준/Gold/25381. ABBC/ABBC.cc
@@ -1,30 +1,31 @@
 #include<iostream>
 #include<algorithm>
 #include<string>
-#include<stack>
+#include<queue>
 using namespace std;
 //#25381_ABBC
-int ans;
+int ans,visited[300001];
 string S;
+queue<int>q;
 int main() {
 	ios_base::sync_with_stdio(0); cout.tie(0); cin.tie(0);
 	cin >> S;
-	int b_cnt = 0, c_cnt = 0;
-	for (int i = S.length()-1; i >= 0; i--) {
-		if (S[i] == 'A') {
-			if (b_cnt > 0) {
-				ans++;
-				b_cnt--;
-			}
+	for (int i = 0; i < S.length(); i++) {
+		if (S[i] == 'B')q.push(i);
+		else if (S[i] == 'C' && !q.empty()) {
+			visited[q.front()] = 1;
+			q.pop();
+			ans++;
 		}
-		if (S[i] == 'B') {
-			if (c_cnt > 0) {
-				ans++;
-				c_cnt--;
-			}
-			else b_cnt++;
+	}
+	int a_cnt = 0;
+	for (int i = 0; i < S.length(); i++) {
+		if (S[i] == 'A')a_cnt++;
+		else if (S[i] == 'B' && a_cnt && !visited[i]) {
+			visited[i] = 1;
+			a_cnt--;
+			ans++;
 		}
-		if (S[i] == 'C') c_cnt++;
 	}
 	cout << ans;
 }
