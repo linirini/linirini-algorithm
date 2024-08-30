5cfc6739f9fcc586d9db241f1b47f53372d011d4
linirini <101927543+linirini@users.noreply.github.com>
2024-08-30 17:12:05 +0900

    [Gold V] Title: 가로등, Time: 296 ms, Memory: 25520 KB, Score: 10 point -BaekjoonHub

---
+++ 백준/Gold/32069. 가로등/가로등.cc
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<algorithm>
+#include<queue>
+#include<set>
+using namespace std;
+//#32069_가로등
+long long L, N, K, k;
+queue<pair<long long, long long>> q;
+set<long long>visited;
+int main() {
+	ios_base::sync_with_stdio(0); cout.tie(0); cin.tie(0);
+	cin >> L >> N >> K;
+	for (int i = 0; i < N; i++) {
+		long long x;
+		cin >> x;
+		q.push({x,0});
+	}
+	while (k!= K) {
+		int now = q.front().first;
+		int cost = q.front().second;
+		q.pop();
+		cout << cost << '\n';
+		k++;
+		visited.insert(now);
+		if (now + 1 >= 0 && now + 1 <= L && visited.find(now + 1) == visited.end()) q.push({ now + 1, cost + 1 });
+		if (now - 1 >= 0 && now - 1 <= L && visited.find(now - 1) == visited.end()) q.push({ now - 1, cost + 1 });
+	}
+}
